f29bf5834bc74be304bc1b7d39867561755ef853
thenotsoft <thenotsoft@gmail.com>
2022-02-03 13:04:01 +0200

    Add JSON_PARTIAL_OUTPUT_ON_ERROR & tests

--- src/Renderer/JsonRenderer.php
+++ src/Renderer/JsonRenderer.php
@@ -19,20 +19,30 @@ final class JsonRenderer implements ThrowableRendererInterface
 {
     public function render(Throwable $t, ServerRequestInterface $request = null): ErrorData
     {
-        return new ErrorData(json_encode([
-            'message' => self::DEFAULT_ERROR_MESSAGE,
-        ], JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES));
+        return new ErrorData(
+            json_encode(
+                [
+                    'message' => self::DEFAULT_ERROR_MESSAGE,
+                ],
+                JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES
+            )
+        );
     }
 
     public function renderVerbose(Throwable $t, ServerRequestInterface $request = null): ErrorData
     {
-        return new ErrorData(json_encode([
-            'type' => get_class($t),
-            'message' => $t->getMessage(),
-            'code' => $t->getCode(),
-            'file' => $t->getFile(),
-            'line' => $t->getLine(),
-            'trace' => $t->getTrace(),
-        ], JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_SUBSTITUTE));
+        return new ErrorData(
+            json_encode(
+                [
+                    'type' => get_class($t),
+                    'message' => $t->getMessage(),
+                    'code' => $t->getCode(),
+                    'file' => $t->getFile(),
+                    'line' => $t->getLine(),
+                    'trace' => $t->getTrace(),
+                ],
+                JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_SUBSTITUTE | JSON_PARTIAL_OUTPUT_ON_ERROR
+            )
+        );
     }
 }
--- tests/Renderer/JsonRendererTest.php
+++ tests/Renderer/JsonRendererTest.php
@@ -19,7 +19,7 @@ final class JsonRendererTest extends TestCase
         $renderer = new JsonRenderer();
         $data = $renderer->render(new RuntimeException());
 
-        $this->assertSame(json_encode(['message' => JsonRenderer::DEFAULT_ERROR_MESSAGE]), (string) $data);
+        $this->assertSame(json_encode(['message' => JsonRenderer::DEFAULT_ERROR_MESSAGE]), (string)$data);
     }
 
     public function testRenderVerbose(): void
@@ -36,7 +36,7 @@ final class JsonRendererTest extends TestCase
             'trace' => $throwable->getTrace(),
         ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
-        $this->assertSame($content, (string) $data);
+        $this->assertSame($content, (string)$data);
     }
 
     public function testRenderVerboseWithNotUtf8String(): void
@@ -53,6 +53,40 @@ final class JsonRendererTest extends TestCase
             'trace' => $throwable->getTrace(),
         ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_SUBSTITUTE);
 
-        $this->assertSame($content, (string) $data);
+        $this->assertSame($content, (string)$data);
+    }
+
+    public function testRenderVerboseWithJsonRecursion(): void
+    {
+        $renderer = new JsonRenderer();
+
+        try {
+            json_encode(
+                new class {
+                    public self $self;
+
+                    public function __construct()
+                    {
+                        $this->self = $this;
+                    }
+                },
+                JSON_THROW_ON_ERROR
+            );
+        } catch (\Throwable $throwable) {
+            $data = $renderer->renderVerbose($throwable);
+            $content = json_encode(
+                [
+                    'type' => \JsonException::class,
+                    'message' => $throwable->getMessage(),
+                    'code' => $throwable->getCode(),
+                    'file' => $throwable->getFile(),
+                    'line' => $throwable->getLine(),
+                    'trace' => $throwable->getTrace(),
+                ],
+                JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_SUBSTITUTE | JSON_PARTIAL_OUTPUT_ON_ERROR
+            );
+
+            $this->assertSame($content, (string)$data);
+        }
     }
 }
