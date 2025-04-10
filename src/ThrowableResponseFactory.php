fb3c5995b73219e8acb811ce74b3ad52103b1c50
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-04-10 15:46:51 +0300

    improve

--- src/ThrowableResponseFactory.php
+++ src/ThrowableResponseFactory.php
@@ -4,33 +4,23 @@ declare(strict_types=1);
 
 namespace Yiisoft\ErrorHandler;
 
-use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Throwable;
-use Yiisoft\ErrorHandler\RendererProvider\CompositeRendererProvider;
-use Yiisoft\ErrorHandler\RendererProvider\ContentTypeRendererProvider;
-use Yiisoft\ErrorHandler\RendererProvider\HeadRendererProvider;
 use Yiisoft\ErrorHandler\RendererProvider\RendererProviderInterface;
 use Yiisoft\Http\Status;
 
 final class ThrowableResponseFactory implements ThrowableResponseFactoryInterface
 {
-    private readonly RendererProviderInterface $rendererProvider;
     private readonly HeadersProvider $headersProvider;
 
     public function __construct(
         private readonly ErrorHandler $errorHandler,
         private readonly ResponseFactoryInterface $responseFactory,
-        ContainerInterface $container,
-        ?RendererProviderInterface $rendererProvider = null,
+        private readonly RendererProviderInterface $rendererProvider,
         ?HeadersProvider $headersProvider = null,
     ) {
-        $this->rendererProvider = $rendererProvider ?? new CompositeRendererProvider(
-            new HeadRendererProvider(),
-            new ContentTypeRendererProvider(container: $container),
-        );
         $this->headersProvider = $headersProvider ?? new HeadersProvider();
     }
 
