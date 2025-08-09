8cfee00a0c28172f643677452818c505270abc59
Dmitrii Derepko <xepozz@list.ru>
2025-08-09 23:54:03 +0300

    Apply suggestion from @xepozz

--- src/Solution/SolutionProviderInterface.php
+++ src/Solution/SolutionProviderInterface.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Yiisoft\ErrorHandler\Solution;
 
+/**
+* The interface declares an adapter to render a solution for an event.
+* Basically, it renders the error message as-is, but possible could render a button with click-to-fix action that will be handled by an HTTP request (with middleware) back to server.
+*/
 interface SolutionProviderInterface
 {
     public function supports(\Throwable $e): bool;
