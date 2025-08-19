07d7c08c2dd94cf56de29f78bcde50f26fafcab1
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-19 10:22:35 +0300

    Fix BC (#154)

--- src/Exception/ErrorException.php
+++ src/Exception/ErrorException.php
@@ -14,16 +14,7 @@ use function function_exists;
 
 /**
  * `ErrorException` represents a PHP error.
- *
- * @psalm-type DebugBacktraceType = list<array{
- *     args?: array<mixed>,
- *     class?: class-string,
- *     file?: string,
- *     function?: string,
- *     line?: int,
- *     object?: object,
- *     type?: string
- * }>
+ * @psalm-type DebugBacktraceType = list<array{args?: array<mixed>, class?: class-string, file?: string, function?: string, line?: int, object?: object, type?: string}>
  *
  * @final
  */
