a7db1f3c59ee66e466acd6953fa1719a729f3509
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-12-24 17:26:45 +0300

    Fix empty first trace item when handling `trigger_error`

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -135,7 +135,12 @@ final class ErrorHandler
             }
 
             $backtrace = debug_backtrace(0);
-            unset($backtrace[0]['function'], $backtrace[0]['class'], $backtrace[0]['type'], $backtrace[0]['args']);
+            if (isset($backtrace[0]['file'])) {
+                unset($backtrace[0]['function'], $backtrace[0]['class'], $backtrace[0]['type'], $backtrace[0]['args']);
+            } else {
+                array_shift($backtrace);
+            }
+
             throw new ErrorException($message, $severity, $severity, $file, $line, null, $backtrace);
         });
 
