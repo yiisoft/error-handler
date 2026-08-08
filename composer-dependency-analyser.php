d9b384d4579df431e70f54effb439c14a6e67bcb
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 08:20:42 +0300

    Add Composer Dependency Analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/config', isDev: false)
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/templates', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    // `psr/event-dispatcher` is an intentionally optional integration: `ErrorHandler` only accepts it
+    // via a nullable constructor type-hint, consumers are not required to install it.
+    ->ignoreErrorsOnPackages(['psr/event-dispatcher'], [ErrorType::DEV_DEPENDENCY_IN_PROD]);
